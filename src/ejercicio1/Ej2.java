b2314afd653f7170bcb4c2798246c5f22d6ad36d
roldanordonez1990 <roldanordonez.francisco@gmail.com>
2022-01-20 17:09:20 +0100

    subido

--- src/ejercicio1/Ej2.java
+++ src/ejercicio1/Ej2.java
@@ -6,7 +6,7 @@ public class Ej2 {
 
 	public static void main(String[] args) {
 		
-		//Ejemplo de STREAM: Son flujos para mostrar datos e interacturar con ellos. Es lo mismo que el Ej1	
+		//Ejemplo de STREAM: Son flujos para mostrar datos e interacturar con ellos. Es lo mismo que el Ej1.	
 		//peek muestra todos los elementos
 		//filter hace el filtro que queramos
 		List<Integer> number = List.of(1, 12, 4, 15, 6, 17);
